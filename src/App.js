25d83ee5069de0e257771c9f69f3f819b035e29e
Vanessa Truo <Supercoder@MacBook-Pro-von-Vanessa.local>
2023-04-06 18:14:27 +0200

    fertig

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+const [count, setCount] = useState(0);
+const [cash, setCash] = useState(0)
+
+const cashChange = (event) => {
+  setCash(Number(event.target.value));
+}
+
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <img src="https://images.unsplash.com/photo-1604148482093-d55d6fc62400?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" alt="banana" id='logoImg'/>
+        <h1 id='title'>Banana Bank</h1>
+        <p className='saldo'>Dein Girokonto:{count}</p>
+      <input type="number" name="" className='geldbetrag' onChange={cashChange} placeholder='Gib einen Geldbetrag ein'/>
+      <button className='einzahlen' onClick={() => setCount(count + cash)}>Einzahlen</button>
+      <button className='auszahlen' onClick={() => setCount(count - cash)}>Auszahlen</button>
+      
+
+
+
+
       </header>
     </div>
   );
